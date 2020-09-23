7a148b8bed1b6521fc9fad382b5505bea4e04b3e
GLegatos <GLegatos@gmail.com>
2020-09-23 09:53:22 -0400

    Updates

---
+++ precache-manifest.ccb013ae5a17648c510c8c6bd161d6e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a040106d19a486a8a64f47509624d0a0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d1a439617d14fe6c4bb6",
+    "url": "/robofriends/static/css/main.71a34315.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a439617d14fe6c4bb6",
+    "url": "/robofriends/static/js/main.0a788396.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "c7d31ee62b1d98e91d97e0fb7cf5feb4",
+    "url": "/robofriends/static/media/Colony Wars.c7d31ee6.woff"
+  }
+]);
